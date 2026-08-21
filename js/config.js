e609ea59d09dc810f4ac297fc567d251e05fd14c
midan4577-afk <midan4577@gmail.com>
2026-08-21 13:10:03 +0300

    Create config.js for deployment settings
    Added configuration for Apps Script Web App deployment.

---
+++ js/config.js
@@ -0,0 +1,12 @@
+/**
+ * config.js
+ * The ONE place a teacher/developer needs to edit after deploying the
+ * Apps Script Web App: paste the deployment URL below.
+ */
+window.APP_CONFIG = {
+  // Paste your Apps Script Web App "exec" URL here, e.g.:
+  // "https://script.google.com/macros/s/AKfycb.../exec"
+  API_URL: 'https://script.google.com/macros/s/AKfycbz-BskedOJYOHGT3ucrEv-z_EjsdhIajhAxYAfykPm8O6kpP2w2SI5Ljn8A6sD-OV_7/exec',
+
+  APP_TITLE: 'תרגול לתלמידים',
+};
